12a26f728927bf9d9e6a48e90ee8ee6d2d9b5967
Ortiz Mauro <mauroortizz191@gmail.com>
2026-07-03 01:42:46 -0300

    agrego todas las funciones CRUD

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -1,14 +1,23 @@
-// (Temporalmente mudamos nuestra base de datos simulada a este universo)
-//uso as para renombrar la funcion, ya que se llaman igual
-import { getAllProducts as getAllProductsModel } from "../models/products.model.js";
-import { addNewProduct as addNewProductModel } from "../models/products.model.js";
+//uso as para renombrar las funciones, ya que se llaman igual
+import { 
+    getAllProducts as getAllProductsModel,
+    addNewProduct as addNewProductModel,
+    getProductById as getProductByIdModel,
+    deleteProductById as deleteProductByIdModel
+    } from "../models/products.model.js";
 
-// uso async porque la funcion del modelo es asincrona
-// ademas al retornar uso el wait porque se que tarda un poco
 export const getAllProducts = async () => {
     return await getAllProductsModel();
 };
 
 export const addNewProduct = async (product) => {
     return await addNewProductModel(product);
+};
+
+export const getProductById = async (id) => {
+    return await getProductByIdModel(id);
+};
+
+export const deleteProductById = async (id) => {
+    return await deleteProductByIdModel(id);
 };
